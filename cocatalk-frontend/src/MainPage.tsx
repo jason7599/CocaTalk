7a1c281b84cbd92c659621801d8d939da80443a9
jason7599 <jason7599@hanmail.net>
2025-12-04 14:08:55 +0900

    FE: load requests order fixed

--- cocatalk-frontend/src/MainPage.tsx
+++ cocatalk-frontend/src/MainPage.tsx
@@ -3,8 +3,15 @@ import Sidebar from "./components/Sidebar";
 import ChatWindow from "./components/ChatWindow";
 import { ModalProvider } from "./context/ModalContext";
 import { UserProvider } from "./context/UserContext";
+import { useEffect } from "react";
+import { usePendingRequestsStore } from "./api/store/pendingRequestsStore";
 
 const MainLayout: React.FC = () => {
+    
+    useEffect(() => {
+        usePendingRequestsStore.getState().fetch();
+    }, []);
+
     return (
         <UserProvider>
         <ModalProvider>
